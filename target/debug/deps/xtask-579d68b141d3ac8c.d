496ec200813531e8a1a591e03bcf91e4740210e0
Michael Pennington <michaeldanielpennington@gmail.com>
2025-12-18 12:55:23 -0500

    remove targets

--- target/debug/deps/xtask-579d68b141d3ac8c.d
+++
@@ -1,7 +0,0 @@
-/home/mpennington/Documents/milkv/duos/rusty-milk/sg2000-rust/target/debug/deps/xtask-579d68b141d3ac8c.d: xtask/src/main.rs
-
-/home/mpennington/Documents/milkv/duos/rusty-milk/sg2000-rust/target/debug/deps/xtask-579d68b141d3ac8c: xtask/src/main.rs
-
-xtask/src/main.rs:
-
-# env-dep:CARGO_MANIFEST_DIR=/home/mpennington/Documents/milkv/duos/rusty-milk/sg2000-rust/xtask
--- target/debug/xtask.d
+++
@@ -1,1 +0,0 @@
-/home/mpennington/Documents/milkv/duos/rusty-milk/sg2000-rust/target/debug/xtask: /home/mpennington/Documents/milkv/duos/rusty-milk/sg2000-rust/xtask/src/main.rs
